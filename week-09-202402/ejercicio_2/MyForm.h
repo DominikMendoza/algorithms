a6ef9bca11434d5f58f38373ab458a22d7ef387b
Dominik Mendoza Ramos <u20201b980@upc.edu.pe>
2024-10-19 12:50:50 -0500

    chore: update week 09 with buffer

--- week-09-202402/ejercicio_2/MyForm.h
+++ week-09-202402/ejercicio_2/MyForm.h
@@ -17,18 +17,22 @@ namespace Project66 {
 	private:
 		CControladora* toxica;
 		Graphics^ graph;
+		BufferedGraphicsContext^ context;
+		BufferedGraphics^ buffer;
 	public:
 		MyForm(void)
 		{
 			InitializeComponent();
 			//srand(time(nullptr));
 			graph = panel1->CreateGraphics();
+			context = BufferedGraphicsManager::Current;
+			buffer = context->Allocate(graph, panel1->ClientRectangle);
 			toxica = new CControladora(graph);
 		}
 
 	protected:
 		/// <summary>
-		/// Limpiar los recursos que se estén usando.
+		/// Limpiar los recursos que se estÃ©n usando.
 		/// </summary>
 		~MyForm()
 		{
@@ -47,14 +51,14 @@ namespace Project66 {
 
 	private:
 		/// <summary>
-		/// Variable del diseñador necesaria.
+		/// Variable del diseÃ±ador necesaria.
 		/// </summary>
 
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Método necesario para admitir el Diseñador. No se puede modificar
-		/// el contenido de este método con el editor de código.
+		/// MÃ©todo necesario para admitir el DiseÃ±ador. No se puede modificar
+		/// el contenido de este mÃ©todo con el editor de cÃ³digo.
 		/// </summary>
 		void InitializeComponent(void)
 		{
@@ -93,10 +97,12 @@ namespace Project66 {
 		}
 #pragma endregion
 	private: System::Void myTimer_Tick(System::Object^ sender, System::EventArgs^ e) {
-		graph->Clear(Color::Gray);
-		toxica->moverTodos(graph);
+		// donde usaba graphics ahora uso buffer
+		buffer->Graphics->Clear(Color::Gray);
+		toxica->moverTodos(buffer->Graphics);
 		toxica->verificarColisiones();
-		toxica->dibujarTodos(graph);
+		toxica->dibujarTodos(buffer->Graphics);
+		buffer->Render(graph);
 	}
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
