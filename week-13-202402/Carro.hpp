1d108713511eafec505554c050f6735bea1135d7
Dominik Mendoza Ramos <u20201b980@upc.edu.pe>
2024-11-09 12:32:44 -0500

    chore: add use case for file manager

--- week-13-202402/Carro.hpp
+++ week-13-202402/Carro.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include "FileManager.hpp"
 using namespace System::Drawing;
 
 class Carro
@@ -8,6 +11,8 @@ private:
 	int x, y, dx;
 	int idx, idy;
 	int width, height;
+	FileManager* fm;
+	vector<Rectangle*> obstaculos;
 public:
 	Carro(int width, int height);
 	~Carro();
@@ -19,11 +24,28 @@ public:
 
 Carro::Carro(int width, int height)
 {
-	x = y = 100;
 	dx = 5;
 	idx = idy = 0;
 	this->width = width;
 	this->height = height;
+
+	fm = new FileManager();
+	string contenido;
+	contenido = fm->readFile("Config.txt");
+	stringstream ss;
+	ss = stringstream(contenido);
+	ss >> x;
+	ss >> y;
+
+	contenido = fm->readFile("Obstaculos.txt");
+	ss = stringstream(contenido);
+	int numeroObstaculos, x, y, ancho, alto;
+	ss >> numeroObstaculos;
+	for (int i = 0; i < numeroObstaculos; i++)
+	{
+		ss >> x >> y >> ancho >> alto;
+		obstaculos.push_back(new Rectangle(x, y, ancho, alto));
+	}
 }
 
 Carro::~Carro()
@@ -50,4 +72,10 @@ void Carro::draw(Graphics^ g, Bitmap^ bmp)
 	Rectangle sectionShow = Rectangle(idx * width, idy * height, width, height);
 	Rectangle sectionForm = Rectangle(x, y, width, height);
 	g->DrawImage(bmp, sectionForm, sectionShow, GraphicsUnit::Pixel);
+
+	SolidBrush^ brush = gcnew SolidBrush(Color::Red);
+	for (int i = 0; i < obstaculos.size(); i++)
+	{
+		g->FillRectangle(brush, *obstaculos[i]);
+	}
 }
